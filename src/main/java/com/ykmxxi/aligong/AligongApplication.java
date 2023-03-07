4ab6a0d8fbc4b48691997fcfb62b2c602b548900
ykmxxi <ykmxxi97@gmail.com>
2023-03-07 11:47:54 +0900

    Project setting
    SpringBoot 2.5.3 + Java 16 + Gradle 7.6.1

---
+++ src/main/java/com/ykmxxi/aligong/AligongApplication.java
@@ -0,0 +1,13 @@
+package com.ykmxxi.aligong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AligongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AligongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ykmxxi/aligong/AligongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykmxxi.aligong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AligongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
