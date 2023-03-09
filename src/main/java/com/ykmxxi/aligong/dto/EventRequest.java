e2a7127e5e4b91f488c90c5a1c78eb28008fc158
ykmxxi <ykmxxi97@gmail.com>
2023-03-09 16:22:07 +0900

    Implement #1- APIEventController, dto

---
+++ src/main/java/com/ykmxxi/aligong/dto/EventRequest.java
@@ -0,0 +1,38 @@
+package com.ykmxxi.aligong.dto;
+
+import java.time.LocalDateTime;
+
+import com.ykmxxi.aligong.constant.EventStatus;
+
+public record EventRequest(
+	Long placeId,
+	String eventName,
+	EventStatus eventStatus,
+	LocalDateTime eventStartDatetime,
+	LocalDateTime eventEndDatetime,
+	Integer currentNumberOfPeople,
+	Integer capacity,
+	String memo
+) {
+	public static EventRequest of(
+		Long placeId,
+		String eventName,
+		EventStatus eventStatus,
+		LocalDateTime eventStartDatetime,
+		LocalDateTime eventEndDatetime,
+		Integer currentNumberOfPeople,
+		Integer capacity,
+		String memo
+	) {
+		return new EventRequest(
+			placeId,
+			eventName,
+			eventStatus,
+			eventStartDatetime,
+			eventEndDatetime,
+			currentNumberOfPeople,
+			capacity,
+			memo
+		);
+	}
+}
