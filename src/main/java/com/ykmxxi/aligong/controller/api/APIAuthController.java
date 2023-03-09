41189d4e50588cd96dee39d69c5bb31ddd90ee41
ykmxxi <ykmxxi97@gmail.com>
2023-03-09 15:45:55 +0900

    Implement #1 - APIAuthController, AdminRequest, LoginRequest
    APIAuthController 기본 입출력 구현, 관리자 회원가입과 로그인 관련 Dto 구현

--- src/main/java/com/ykmxxi/aligong/controller/api/APIAuthController.java
+++ src/main/java/com/ykmxxi/aligong/controller/api/APIAuthController.java
@@ -1,20 +1,25 @@
 package com.ykmxxi.aligong.controller.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ykmxxi.aligong.dto.APIDataResponse;
+import com.ykmxxi.aligong.dto.AdminRequest;
+import com.ykmxxi.aligong.dto.LoginRequest;
 
 @RequestMapping("/api")
 @RestController
 public class APIAuthController {
 
-	@GetMapping("/sign-up")
-	public String signUp() {
-		return "done.";
+	@PostMapping("/sign-up")
+	public APIDataResponse<String> signUp(@RequestBody AdminRequest adminRequest) {
+		return APIDataResponse.empty();
 	}
 
-	@GetMapping("/login")
-	public String login() {
-		return "done.";
+	@PostMapping("/login")
+	public APIDataResponse<String> login(@RequestBody LoginRequest loginRequest) {
+		return APIDataResponse.empty();
 	}
 }
