ed72bd2ce48f2dc97dbcdd583d41cca24dc63b08
ykmxxi <ykmxxi97@gmail.com>
2023-03-09 16:10:07 +0900

    Style #1- optimize imports

--- src/test/java/com/ykmxxi/aligong/controller/PlaceControllerTest.java
+++ src/test/java/com/ykmxxi/aligong/controller/PlaceControllerTest.java
@@ -1,14 +1,14 @@
 package com.ykmxxi.aligong.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PlaceController.class)
 class PlaceControllerTest {
--- src/test/java/com/ykmxxi/aligong/error/BaseErrorControllerTest.java
+++ src/test/java/com/ykmxxi/aligong/error/BaseErrorControllerTest.java
@@ -1,7 +1,5 @@
 package com.ykmxxi.aligong.error;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -10,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(BaseErrorController.class)
