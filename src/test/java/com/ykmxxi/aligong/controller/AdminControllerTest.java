63cd2c514f3d96c442e6827837ce82639c2092a5
ykmxxi <ykmxxi97@gmail.com>
2023-03-09 15:30:40 +0900

    Test #1- AdminController 테스트 구현
    관리자 컨트롤러 테스트 구현

---
+++ src/test/java/com/ykmxxi/aligong/controller/AdminControllerTest.java
@@ -0,0 +1,91 @@
+package com.ykmxxi.aligong.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.ykmxxi.aligong.constant.EventStatus;
+import com.ykmxxi.aligong.constant.PlaceType;
+
+@WebMvcTest(AdminController.class)
+class AdminControllerTest {
+
+	private final MockMvc mvc;
+
+	public AdminControllerTest(@Autowired MockMvc mvc) {
+		this.mvc = mvc;
+	}
+
+	@DisplayName("[view][GET] 어드민 페이지 - 장소 리스트 뷰")
+	@Test
+	void givenQueryParams_whenRequestingAdminPlacesPage_thenReturnsAdminPlacesPage() throws Exception {
+		// Given
+
+		// When & Then
+		mvc.perform(
+				get("/admin/places")
+					.queryParam("placeType", PlaceType.SPORTS.name())
+					.queryParam("placeName", "치평테니스코트")
+					.queryParam("address", "광주광역시 서구 시청로 11")
+			)
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+			.andExpect(view().name("admin/places"));
+	}
+
+	@DisplayName("[view][GET] 어드민 페이지 - 장소 세부 정보 뷰")
+	@Test
+	void givenPlaceId_whenRequestingAdminPlaceDetailPage_thenReturnsAdminPlaceDetailPage() throws Exception {
+		// Given
+		long placeId = 1L;
+
+		// When & Then
+		mvc.perform(get("/admin/places/" + placeId))
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+			.andExpect(view().name("admin/place-detail"));
+	}
+
+	@DisplayName("[view][GET] 어드민 페이지 - 이벤트 리스트 뷰")
+	@Test
+	void givenQueryParams_whenRequestingAdminEventsPage_thenReturnsAdminEventsPage() throws Exception {
+		// Given
+
+		// When & Then
+		mvc.perform(
+				get("/admin/events")
+					.contentType(MediaType.TEXT_HTML)
+					.queryParam("placeId", "1")
+					.queryParam("placeName", "치평테니스코트")
+					.queryParam("eventName", "오후 운동")
+					.queryParam("eventStatus", EventStatus.OPENED.name())
+					.queryParam("eventStartDatetime", LocalDateTime.now().minusDays(1).toString())
+					.queryParam("eventEndDatetime", LocalDateTime.now().toString())
+			)
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+			.andExpect(view().name("admin/events"));
+	}
+
+	@DisplayName("[view][GET] 어드민 페이지 - 이벤트 세부 정보 뷰")
+	@Test
+	void givenEventId_whenRequestingAdminEventDetailPage_thenReturnsAdminEventDetailPage() throws Exception {
+		// Given
+		long eventId = 1L;
+
+		// When & Then
+		mvc.perform(get("/admin/events/" + eventId))
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+			.andExpect(view().name("admin/event-detail"));
+	}
+
+}
